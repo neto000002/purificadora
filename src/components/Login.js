cd08d810bd12aa8aeea179d882339353d5c2dec8
Neto <dominguezernesto222@gmail.com>
2025-06-27 18:38:17 -0600

    Actualizaciones al frontend con conexión a backend en Render

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-    console.log('API URL:', process.env.REACT_APP_API_URL);
+    console.log("API URL:", process.env.REACT_APP_API_URL);
     const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
